4c6eedc558b15e09be38e74e0e7207ce4679dc09
Deven Smith <DevenSmith@users.noreply.github.com>
2022-02-21 10:45:28 -0800

    Update ArmController.cs
    forgot to put it in the right namespace

--- SunriseRiders/Assets/Scripts/Game/Characters/GameInput/ArmController.cs
+++ SunriseRiders/Assets/Scripts/Game/Characters/GameInput/ArmController.cs
@@ -1,35 +1,32 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Game.Characters.GameInput;
-using UnityEngine;
-using UnityEngine.Animations;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 
-/// <summary>
-/// for now it follows the mouse later consider having a second joystick to control arm direction.
-/// </summary>
-public class ArmController : MonoBehaviour
+namespace Game.Characters.GameInput
 {
-    [SerializeField] private PlayerInput playerInput;
-    [SerializeField] private Transform armTransform;
-    public void Start()
+    /// <summary>
+    /// for now it follows the mouse later consider having a second joystick to control arm direction.
+    /// </summary>
+    public class ArmController : MonoBehaviour
     {
-        if (playerInput == null)
+        [SerializeField] private PlayerInput playerInput;
+        [SerializeField] private Transform armTransform;
+        public void Start()
         {
-            playerInput = GetComponent<PlayerInput>();
             if (playerInput == null)
             {
-                Debug.LogError(gameObject.name + " armController doesn't have a player input");
+                playerInput = GetComponent<PlayerInput>();
+                if (playerInput == null)
+                {
+                    Debug.LogError(gameObject.name + " armController doesn't have a player input");
+                }
             }
+
+            if (armTransform == null)
+                armTransform = transform;
         }
 
-        if (armTransform == null)
-            armTransform = transform;
-    }
-
-    public void Update()
-    {
-        armTransform.rotation = Quaternion.FromToRotation(Vector3.right, playerInput.ArmDirection);
+        public void Update()
+        {
+            armTransform.rotation = Quaternion.FromToRotation(Vector3.right, playerInput.ArmDirection);
+        }
     }
 }
