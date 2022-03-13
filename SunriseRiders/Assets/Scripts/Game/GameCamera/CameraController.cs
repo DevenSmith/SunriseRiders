125ed91b12e1dd055b8b62c9d835b3e564714b70
Deven Smith <DevenSmith@users.noreply.github.com>
2022-03-12 20:00:43 -0800

    Basic follow camera and level set up
    Will need a better camera later but this will do for now.

---
+++ SunriseRiders/Assets/Scripts/Game/GameCamera/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Game.GameCamera
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform playerTransform;
+
+        private Transform _cameraTransform;
+        private Vector3 _cameraPosition;
+        private void Awake()
+        {
+            _cameraTransform = transform;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _cameraPosition = _cameraTransform.position;
+            _cameraPosition.x = playerTransform.position.x;
+            _cameraTransform.position = _cameraPosition;
+        }
+    }
+}
