a3897faaa3993fd0af466f6a3663da839e4cac16
Deven Smith <Deven@hashbanggames.com>
2023-11-15 21:50:37 -0800

    Added sprites for main menu ui and functionality

---
+++ SunriseRiders/Assets/Scripts/Game/LoadLevelComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using Devens;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game
+{
+    public class LoadLevelComponent : MonoBehaviour
+    {
+        [SerializeField] private StringSO levelNameStringSo;
+
+        private Coroutine loadSceneAsyncRoutine = null;
+        
+        [UsedImplicitly]
+        public void LoadLevel()
+        {
+            if (loadSceneAsyncRoutine == null)
+            {
+                loadSceneAsyncRoutine = StartCoroutine(LoadAsyncScene());
+            }
+        }
+        
+        private IEnumerator LoadAsyncScene()
+        {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelNameStringSo.Value);
+
+            // Wait until the asynchronous scene fully loads
+            while (!asyncLoad.isDone)
+            {
+                yield return null;
+            }
+        }
+    }
+}
